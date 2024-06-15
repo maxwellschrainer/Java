7593e517326fd2784470d441d9fcd513da34e29a
Maxwell Schrainer <143411689+maxwellschrainer@users.noreply.github.com>
2024-06-15 13:35:20 -0300

    ex1

--- Faculdade/forumex001.java
+++ Faculdade/forumex001.java
@@ -1,6 +1,30 @@
 package Faculdade;
+
 import java.util.Scanner;
-public class forumex001
-{
-    public static void main(String[] args)
+
+public class forumex001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1, num2, soma, subtração, multiplicação;
+
+        System.out.println("Digite o primeiro número:");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número");
+        num2 = scanner.nextDouble();
+
+        scanner.close();
+
+        soma = num1 + num2;
+        subtração = num1 - num2;
+        multiplicação = num1 * num2;
+
+        System.out.println("A soma dos dois números é: " + soma);
+
+        System.out.println("A subtração dos dois números é: " + subtração);
+
+        System.out.println("A multiplicação dos dois números é: " + multiplicação);
+
+    }
 }

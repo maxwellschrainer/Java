168445a7aa629cf5ffa17965f2582a05a832eb2f
Maxwell Schrainer <143411689+maxwellschrainer@users.noreply.github.com>
2024-06-16 15:26:37 -0300

    save

--- Faculdade/forumex004.java
+++ Faculdade/forumex004.java
@@ -3,5 +3,10 @@ package Faculdade;
 import java.util.Scanner;
 
 public class forumex004 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
 }

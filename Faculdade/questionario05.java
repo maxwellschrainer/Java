7a9848b673b0b93449eff22d9b540a5707374e1d
Maxwell Schrainer <143411689+maxwellschrainer@users.noreply.github.com>
2024-06-18 23:18:04 -0300

    qs

--- Faculdade/questionario05.java
+++ Faculdade/questionario05.java
@@ -3,23 +3,29 @@ package Faculdade;
 import java.util.Scanner;
 
 public class questionario05 {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         String nome = "João";
         int senha = 10121985;
         int diaNascimento = 10;
         int mesNascimento = 12;
         int anoNascimento = 1985;
-        int dataNascimento = diaNascimento + mesNascimento + anoNascimento, senhaDigitada; 
+        int dataNascimento = diaNascimento + mesNascimento + anoNascimento, senhaDigitada;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite sua senha numérica de 8 dígitos:");
         senhaDigitada = scanner.nextInt();
 
-        if(senha == senhaDigitada){
+        if (senha == senhaDigitada) {
             System.out.println("Seu acesso foi liberado");
+            if (senha == dataNascimento) {
+                System.out.println("Senha insegura, por favor, troque sua senha.");
+            } else {
+                System.out.println("Sua senha está incorreta");
+            }
         }
     }
-    
+
 }

ba822ef8d7ea8582984667527188b74ade55cfa3
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2024-06-19 10:13:24 -0300

    update

--- Faculdade/questionario05.java
+++ Faculdade/questionario05.java
@@ -3,30 +3,30 @@ package Faculdade;
 import java.util.Scanner;
 
 public class questionario05 {
-    public static void main(String[] args){
-        
-        Scanner scanner = new Scanner(System.in);
 
-        int senha = 10121985;
-        int diaNascimento = 10;
-        int mesNascimento = 12;
-        int anoNascimento = 1985;
-        int dataNascimento = diaNascimento + mesNascimento + anoNascimento, senhaDigitada; 
+    public static void main(String[] args) {
 
-        System.out.println("Digite sua senha numérica de 8 dígitos:");
-        senhaDigitada = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        scanner.close();
+            int senha = 10121985;
+            int diaNascimento = 10;
+            int mesNascimento = 12;
+            int anoNascimento = 1985;
+            int dataNascimento = diaNascimento + mesNascimento + anoNascimento, senhaDigitada;
 
-        if(senha == senhaDigitada){
-            System.out.println("Seu acesso foi liberado");
-        if(senha == dataNascimento){
-            System.out.println("Senha insegura, por favor, troque sua senha.");
-            } 
-            else{
-                System.out.println("Sua senha está incorreta");
+            System.out.println("Digite sua senha numérica de 8 dígitos:");
+            senhaDigitada = scanner.nextInt();
+
+            scanner.close();
+
+            if (senha == senhaDigitada) {
+                System.out.println("Seu acesso foi liberado");
+                if (senha == dataNascimento) {
+                    System.out.println("Senha insegura, por favor, troque sua senha.");
+                } else {
+                    System.out.println("Sua senha está incorreta");
+                }
             }
         }
     }
-    
 }

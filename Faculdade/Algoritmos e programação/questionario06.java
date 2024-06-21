1988b5a18288778d2a6d7a6a7e1202c67e807005
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2024-06-21 15:42:11 -0300

    updt

---
+++ Faculdade/Algoritmos e programação/questionario06.java
@@ -0,0 +1,10 @@
+
+public class questionario06 {
+
+    public static void main(String[] args) {
+        int cont;
+        for (cont = 1; cont <= 10; cont++) {
+        }
+        System.out.println(cont);
+    }
+}
---
+++ Faculdade/Algoritmos e programação/questionario07.java
@@ -0,0 +1,11 @@
+
+public class questionario07 {
+
+    public static void main(String[] args) {
+        int i = 2;
+        while (i < 6) {
+            i = i + 1;
+            System.out.println("A vaca morreu");
+        }
+    }
+}

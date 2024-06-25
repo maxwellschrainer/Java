0cbdaf632f9c6ba6d562a4a53834b2788c059872
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2024-06-25 09:43:06 -0300

    a

--- Faculdade/Algoritmos e programação/questionario10.java
+++ Faculdade/Algoritmos e programação/questionario10.java
@@ -2,7 +2,7 @@
 public class questionario10 {
     public static void main(String[] args) {
         int i = 2;
-        while (i < 22) {
+        while (i < 12) {
             i = i + 3;
             System.out.println(i + 2);
         }
---
+++ Faculdade/Algoritmos e programação/questionario11.java
@@ -0,0 +1,11 @@
+
+public class questionario11 {
+
+    public static void main(String[] args) {
+        int cont;
+        for (cont = 1; cont <= 10; cont++) {
+        
+            System.out.println(cont);
+        }
+    }
+}

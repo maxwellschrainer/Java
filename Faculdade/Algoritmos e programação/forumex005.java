8bb103312e53b8e2cc62a1dbe79e00fecab59b1c
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2024-07-04 11:46:36 -0300

    graxa

--- Faculdade/Algoritmos e programação/forumex005.java
+++ Faculdade/Algoritmos e programação/forumex005.java
@@ -1,30 +1,22 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class forumex005 {
-    public static void main(String[] args){
-        try (Scanner scanner = new Scanner(System.in)){
 
-            float nota1, nota2;
+    public static void main(String[] args) {
+        ArrayList<Float> notas = new ArrayList();
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            float nota1;
 
             System.out.println(" Digite a sua primeira nota.");
             nota1 = scanner.nextFloat();
 
-            if (nota1 > 100){
-            System.out.println("Sua nota não pode ser maior do que 100, digite novamente.");
+            if (nota1 >= 0 && nota1 <= 100) {
+                notas.add(nota1);
             }
-            nota1 = scanner.nextFloat();
-
-            System.out.println("Digite a sua segunda nota");
-            nota2 = scanner.nextFloat();
-
-            if (nota2 > 100){
-                System.out.println("Sua nota não pode ser maior do que 100, digite novamente.");
+        
             }
-            nota2 = scanner.nextFloat();
-
-            System.out.println("Sua primeira nota foi: " + nota1);
-
-            System.out.println("E sua segunda nota foi: " + nota2);
         }
     }
-}
